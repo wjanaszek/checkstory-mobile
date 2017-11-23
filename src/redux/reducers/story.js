8acbd2fa18a11ec9ccd7f8f035fe51d8a6750f0e
wjanaszek <trash.wjanaszek@gmail.com>
2017-11-23 20:52:44 +0100

    Reducer for story state

---
+++ src/redux/reducers/story.js
@@ -0,0 +1,13 @@
+const initialState = {
+  story: Story = null,
+  photos: Photo = [],
+  loading: false
+};
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
